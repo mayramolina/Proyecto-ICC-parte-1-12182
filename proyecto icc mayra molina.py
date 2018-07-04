39cab93d425fa6e27d851f354d8b57b41545bf18
mayramolina <40770716+mayramolina@users.noreply.github.com>
2018-07-04 12:04:32 -0500

    Update proyecto icc mayra molina.py

--- proyecto icc mayra molina.py
+++ proyecto icc mayra molina.py
@@ -1,14 +1,21 @@
-#Carta
-POSTRE = ["Flan","Mazamorra","Pudin","P4"]
-ENTRADA = ["E1","E2","E3","E4"]
-SEGUNDO = ["S1","S2","S3","S4"]
-BEBIDA = ["B1","B2","B3","B4"]
-#M=Mesero
-#C=Cliente
+#carta
+POSTRE = ["Flan","Mazamorra","Pudin","Supiro a la limeña"]
+ENTRADA = ["Papa a la huacaína ","Ocopa","Papa rellena","Anticuhos"]
+SEGUNDO = ["Aji de Gallina","Chanfainita","Arroz con Pollo","Bistec a lo Pobre"]
+BEBIDA = ["Inka Kola","Chicha Morada","Chicha de Jora","Coca Cola"]
 
-#Funciones
+hora=input("por último ingrese que hora es (horaminutos):")
+if int(hora) < 900:
+ print("El restaurante aun no esta abierto, regrese más tarde")
+elif int(hora) >= 900:
+  print("El restaurante esta abierto, pase por favor")
+clientes=int(input("Ingrese el número de clientes el día de hoy:"))
+if int(hora)>= 900:
+    for x in range (clientes):
+        print("[Entorno]: El cliente deja su auto con el valet parking")
+        auto_modelo=input("Porfavor, me podría decir el modelo de su auto:")
+        placa=input("Y su número de placa:")
 
-#Codigo
 print("[Entorno]: El cliente llega al restaurante y se siente.")
 print("[Entorno]: El mesero se acerca y le da la bienvenida al restaurante.")
 print("[Mesero]: Bienvenidos a Coco's restaurant")
